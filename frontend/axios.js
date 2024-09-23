25af0f4ada76ef949b2d46c4beed5ff4478d2ec1
Shekel Roman <romansekel37@gmail.com>
2024-09-23 21:59:57 +0200

    server domen fix

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'http://localhost:8000',
+    baseURL:'https://events-1-nbr4.onrender.com',
 })
 
 export const getAllEvents = async (page = 1, perPage = 12, fields = '') => {
