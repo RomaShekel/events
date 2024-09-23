f9de03d5c5f2254b32548a0cce2a71444d3cc3cb
Shekel Roman <romansekel37@gmail.com>
2024-09-23 22:13:03 +0200

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ export const setupServer = () => {
     const app = express();
 
     const corsOptions = {
-        origin:['http://localhost:5173', 'https://events-theta-two.vercel.app/'],
+        origin:['http://localhost:5173', 'https://events-theta-two.vercel.app'],
     }
 
     app.use(cors(corsOptions))
